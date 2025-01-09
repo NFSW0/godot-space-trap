27a8d4fc10f09bf2c49357fd20d12b01df77d028
qinjunyue <qinjunyue@duiba.com.cn>
2025-01-09 18:56:14 +0800

    传送门节点结构调整

--- SpaceTrap/world/portal/portal.gd
+++ SpaceTrap/world/portal/portal.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
 
-@export var targe_portal:Node
+# 传递body、self、target_portal给处理器
+
+@export var targe_portal:Area2D
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	# 传递body、self、target_portal给处理器
+func _on_area_entered(area: Area2D) -> void:
 	pass # Replace with function body.
 
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	# 传递area、self、target_portal给处理器
+func _on_body_entered(body: Node2D) -> void:
 	pass # Replace with function body.
