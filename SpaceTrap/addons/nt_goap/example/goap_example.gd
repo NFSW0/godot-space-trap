5fc7b87be344c7537c19eb4a1fa16aac9400f278
NFSW0 <1262510555@qq.com>
2025-02-22 14:34:26 +0800

    save

--- SpaceTrap/addons/nt_goap/example/goap_example.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-
-
-@export var goap: GOAP
-
-
-func _physics_process(delta: float) -> void:
-	goap.execute(delta, self)
--- SpaceTrap/addons/nt_goap/nodes/goap_action/goap_move_2d/goap_move_2d.gd
+++
@@ -1,6 +0,0 @@
-extends GOAPAction
-class_name GOAPMove2D
-
-
-func execute(delta:float, entity):
-	pass
