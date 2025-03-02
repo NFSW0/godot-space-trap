8c88ffbc2f16b4316dc76a562aee1fc946c58451
NFSW0 <1262510555@qq.com>
2025-03-02 11:41:15 +0800

    save

--- SpaceTrap/entity/entity_library/5_skeleton/skeleton.gd
+++ SpaceTrap/entity/entity_library/5_skeleton/skeleton.gd
@@ -18,19 +18,16 @@ func travel_animation(animation_name: String):
 		animation_tree.get("parameters/playback").travel(animation_name)
 
 
-## 设置智能逻辑
+#region 智能
 func _set_brain():
-	# 初始化AI大脑示例
 	var ai_brain = GOAP_AIBrain.new()
 	# 设置世界状态更新方法
 	ai_brain.world_update = func():
 		return {
-			"target": _perceptual(), # 获取区域内其他节点，选择合适目标作为处理对象
-			"target_position": Vector2(100, 200),
+			"target": _perceptual(),
 			"health": 80.0,
-			"ammo": 5
 		}
-	# 添加目标
+	# 添加目标(不同对象可以有不同目标)
 	ai_brain.current_goals.append(
 		GOAP_AIBrain.GOAPGoal.new(
 			"AttackTarget",
@@ -42,22 +39,23 @@ func _set_brain():
 	ai_brain.available_actions.append(
 		GOAP_AIBrain.GOAPAction.new(
 			"MoveToTarget",
-			{"target_position": func(v): return v.distance_to(position) > navigation_agent_2d.target_desired_distance},
-			{"in_range": true},
+			{"target": func(v): return position.distance_to(v.position) > navigation_agent_2d.target_desired_distance if v and v.get("position") else false},
+			{"target_in_range": true},
 			1.0,
-			func(state): return {ControllerBase.COMMAND_TYPE.MOVE_TO: state["target_position"]}
+			func(state): return {ControllerBase.COMMAND_TYPE.MOVE_TO: state["target"].get("position")}
 		)
 	)
 	ai_brain.available_actions.append(
 		GOAP_AIBrain.GOAPAction.new(
-			"ShootTarget",
-			{"in_range": true, "ammo": func(v): return v > 0},
+			"AttackTarget",
+			{"target_in_range": true},
 			{"target_destroyed": true},
 			2.0,
-			func(state): return {ControllerBase.COMMAND_TYPE.ATTACK: true}
+			func(_state): return {ControllerBase.COMMAND_TYPE.ATTACK: true}
 		)
 	)
 	controller.set("ai_brain", ai_brain)
+#endregion 智能
 
 
 #region 感知
@@ -66,7 +64,7 @@ func _perceptual() -> Node:
 	var nodes_in_area = perceptron.get_overlapping_areas() + perceptron.get_overlapping_bodies()
 	# 排除无关对象
 	nodes_in_area = nodes_in_area.filter(func(element): return element.is_in_group("Player"))
-	if nodes_in_area.empty():
+	if nodes_in_area.is_empty():
 		return null
 	nodes_in_area.sort_custom(_compare_priority)
 	return nodes_in_area[0]
@@ -75,10 +73,7 @@ func _compare_priority(a: Node, b: Node) -> int:
 	# 近距离优先
 	var dist_a = position.distance_to(a.position)
 	var dist_b = position.distance_to(b.position)
-	if dist_a < dist_b:
-		return true
-	else:
-		return false
+	return dist_a < dist_b
 #endregion 感知
 
 
@@ -97,7 +92,6 @@ func _execute_command(command: Dictionary) -> void:
 func _move_to(data: Vector2 = Vector2()) -> void:
 	if navigation_agent_2d:
 		var map_rid = navigation_agent_2d.get_navigation_map()
-		var agent_rid = navigation_agent_2d.get_rid()
 		navigation_agent_2d.target_position = data
 		# 已更新且未抵达最终位置
 		if NavigationServer2D.map_get_iteration_id(map_rid) and not navigation_agent_2d.is_navigation_finished():
@@ -107,6 +101,6 @@ func _move_to(data: Vector2 = Vector2()) -> void:
 
 
 ## 攻击
-func _attack(data = null)-> void:
+func _attack(_data = null)-> void:
 	print("骷髅发动攻击")
 #endregion 行动
