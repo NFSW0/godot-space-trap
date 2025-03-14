5416533b8cbf5dcd8b7f65d096a97d9301fe599a
NFSW <1262510555@qq.com>
2025-03-14 18:53:49 +0800

    save
    在特效节点中添加获取在影响范围内的实体的方法，生成的特效无法获取考虑换成特效造成伤害

--- SpaceTrap/entity/entity_library/6_effect_hit/effect_hit.gd
+++ SpaceTrap/entity/entity_library/6_effect_hit/effect_hit.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 @export var entity_id:int = 0
 @export var particles:GPUParticles2D
+@export var area_2d: Area2D
 
 
 func _ready() -> void:
@@ -14,6 +15,6 @@ func _on_finished() -> void:
 	call_deferred("queue_free")
 
 
-## 近战攻击获取伤害区域
-func get_range():
-	pass
+## 获取在影响范围内的实体
+func get_the_entities_in_the_influence_area():
+	return area_2d.get_overlapping_bodies()
