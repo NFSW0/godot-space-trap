0bcae65f7aee686b2ba903ecc7fef06e3b6f90fe
NFSW <1262510555@qq.com>
2025-04-22 14:52:56 +0800

    亚空间搁置

--- SpaceTrap/MAIN/2_game_scene/game_scene_2.gd
+++ SpaceTrap/MAIN/2_game_scene/game_scene_2.gd
@@ -8,16 +8,38 @@ const WORLD_SCENE = "res://world/world.tscn"
 
 
 func _ready() -> void:
-	#$Node2D/Entities/Dummy/Camera2D/SubViewportContainer/SubViewport.world_2d = get_tree().root.world_2d
+	$Node2D/Entities/Dummy/Camera2D/SubViewportContainer/SubViewport.world_2d = get_tree().root.world_2d
 	get_tree().root.set_canvas_cull_mask_bit(5, false)
+	var material = $Node2D/Entities/Dummy/Camera2D/SubViewportContainer.get("material")
+	var image_multiplier = material.get("shader_parameter/image_multiplier")
+	var tween = create_tween()
+	tween.tween_property(image_multiplier, "width", 64.0, 2.0 )
+	tween.parallel().tween_property(image_multiplier, "height", 64.0, 2.0 )
+	Dialogic.signal_event.connect(on_chat_end)
 	if entity_manager:
 		# 设置多人实体承载节点
 		entity_manager.update_spawn_path($Node2D/Entities.get_path())
 
 
-func _process(_delta: float) -> void:
-	$Node2D/Entities/Dummy/Camera2D/SubViewportContainer/SubViewport/Camera2D.position = $Node2D/Entities/Dummy.position
+func on_chat_end(arg: String):
+	if arg == "End":
+		var material = $Node2D/Entities/Dummy/Camera2D/SubViewportContainer.get("material")
+		var image_multiplier = material.get("shader_parameter/image_multiplier")
+		var tween = create_tween()
+		tween.tween_property(image_multiplier, "width", 1.0, 2.0 )
+		tween.parallel().tween_property(image_multiplier, "height", 1.0, 2.0 )
+		tween.tween_callback(func():$Node2D/Entities/Dummy/Camera2D/SubViewportContainer.queue_free())
 
+
+func _process(_delta: float) -> void:
+	# 获取 Dummy 节点
+	var dummy = get_node_or_null("Node2D/Entities/Dummy")
+	# 获取 Camera2D 节点
+	var camera = get_node_or_null("Node2D/Entities/Dummy/Camera2D/SubViewportContainer/SubViewport/Camera2D")
+	# 检查节点是否存在
+	if dummy and camera:
+		# 确保两个节点都存在时才进行赋值
+		camera.position = dummy.position
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.as_text() == "Escape":
